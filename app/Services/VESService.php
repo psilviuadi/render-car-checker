23f0f5263f1b83355821bf8b1cae3bc0a3c24a6b
Adi Pruteanu <p.silviuadi@gmail.com>
2025-09-09 08:59:47 +0100

    Make actual API calls

--- app/Services/VESService.php
+++ app/Services/VESService.php
@@ -8,7 +8,6 @@ class VESService
 {
     public function lookup(string $registrationNumber): array
     {
-        return json_decode('{"registrationNumber":"PY06FTD","taxStatus":"Taxed","taxDueDate":"2025-11-01","motStatus":"Valid","make":"AUDI","yearOfManufacture":2006,"engineCapacity":1986,"co2Emissions":169,"fuelType":"DIESEL","markedForExport":false,"colour":"BLACK","typeApproval":"M1","dateOfLastV5CIssued":"2024-11-01","motExpiryDate":"2026-05-09","wheelplan":"2 AXLE RIGID BODY","monthOfFirstRegistration":"2006-05"}', true);
         $response = Http::withHeaders([
             'x-api-key' => config('ves.key'),
             'Content-Type' => 'application/json',
